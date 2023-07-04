804b8079728701db5ff867a4a6e475b83b914204
Jwa Regian <109281517+JwaSales1@users.noreply.github.com>
2023-07-04 19:40:06 -0300

    Add files via upload
    1. Desenvolva uma página HTML que, ao ser carregada, exibe um alerta informando que acabou de ser carregada.
    Exemplo: Página carregada com sucesso!
    
    
    
    2. Declare duas variáveis, coloque um valor em cada uma e faça:
    uma soma;
    uma divisão;
    uma multiplicação;
    uma comparação para ver se os valores são iguais (faça outros testes para treinar também!).

---
+++ Váriaveis/variaveis.js
@@ -0,0 +1,20 @@
+
+
+var x = 6
+var y = 9
+var z = 3
+
+console.log(x + y)
+console.log(y / z)
+console.log(y % x)
+console.log(x * z)
+
+console.log(x + z * y % z)
+
+console.log(y == x)
+console.log(z != y)
+
+
+
+    
+   
