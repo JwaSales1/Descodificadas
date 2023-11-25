4ece294ba96e1bcecf88b93f12e11925f410b183
Jwa Regian <109281517+JwaSales1@users.noreply.github.com>
2023-11-24 22:54:55 -0300

    Add files via upload
    Tarefa 10 - Funções
    
    Faça um projeto que contenha uma função JavaScript em que o usuário informe 5 números e o programa calcule a média aritmética desses números e mostre o resultado.
    
    
    Envie o link do repositório do GitHub (como hiperlink) com seu projeto.

---
+++ função-Javascript/função.js
@@ -0,0 +1,21 @@
+var num1 = 5;
+var num2 = 10;
+var num3 = 80;
+var num4 = 2;
+var num5 = 15;
+
+function numero(){
+   //return num1 * num2;
+   console.log(num1*num2)
+   console.log(num3/num4)
+   console.log(num5-num3)
+   console.log(num3+num4)
+   console.log(num1+num2*num3-num4/num5)
+
+
+}numero();
+
+
+
+
+
