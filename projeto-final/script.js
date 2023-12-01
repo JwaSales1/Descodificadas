cd8755622c9abb6a29b3197adbb982ee2856425a
Jwa Regian <109281517+JwaSales1@users.noreply.github.com>
2023-11-30 22:41:28 -0300

    Add files via upload
    Projeto final da trilha 2 - descodificadas-.

---
+++ projeto-final/script.js
@@ -0,0 +1,10 @@
+//Expandir o menu
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
+
+//formulário
+
